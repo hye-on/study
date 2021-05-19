39501ad3b8ffb48b2cfb1c605d286f1ba9d938da
minj-L <52521457+minj-L@users.noreply.github.com>
2021-05-20 02:16:41 +0900

    Update no.1026_보물(민지).py

--- no.1026_보물(민지).py
+++ no.1026_보물(민지).py
@@ -4,9 +4,8 @@ b_list = list(map(int, input().split()))
 
 s = 0
 for i in range(n):
-    
+    s += min(a_list) * max(b_list)
     a_list.pop(a_list.index(min(a_list))) #a_list의 가장 작은 값을 꺼냄
     b_list.pop(b_list.index(max(b_list))) #b_list의 가장 큰 값을 꺼냄
-    s += min(a_list) * max(b_list)
 
 print(s)
