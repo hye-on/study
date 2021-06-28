398a13ce3bd480cca32b645dcab2bd9370417d23
minj-L <52521457+minj-L@users.noreply.github.com>
2021-06-28 21:08:58 +0900

    Update 소트인사이드.cpp

--- 주민지/소트인사이드.cpp
+++ 주민지/소트인사이드.cpp
@@ -10,7 +10,7 @@ int main()
 
     for (int i = 0; i < N.length(); i++)
     {
-        for (int j = 0; j <N.length() - i - 1; j++)
+        for (int j = 0; j <N.length() - i - 1; j++) //index값을 맞추기 위함
         {
             if (N[j] < N[j + 1]) //만약 앞의 숫자가 뒤의 숫자보다 작다면
             {
