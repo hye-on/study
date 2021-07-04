ef6ac544f3b01b8f4599fc321849682b4da8f119
minj-L <52521457+minj-L@users.noreply.github.com>
2021-07-04 22:26:27 +0900

    Update 부분수열의 합.py

--- 주민지/부분수열의 합.py
+++ 주민지/부분수열의 합.py
@@ -1,19 +1,26 @@
 def f(x, d):
-    global res
-    if(x >= n):
-        if(s == d):
+    global res #함수 밖에서도 사용 가능한 전역 변수
+    if(x >= N):
+        if(S == d):
             res += 1
             return
     else:
-        f(x+1, d+arr[x])
+        f(x+1, d+arr[x]) #x값 증가시켜가면서 함수 반복
+        #f 함수가 원소를 포함하는 경우
         f(x+1, d)
+        #f 함수가 원소를 포함하지 않는 경우
 
-n, s = map(int, input().split())
+N, S = map(int, input().split())
+while N > 20 or N < 1 or abs(S) > 100000:
+    print("N은 1<= N <= 20 이며, S의 절댓값은 10,000을 넘지 않는다.")
 arr = list(map(int, input().split()))
+
 res = 0
 f(0,0)
 
-if(s):
+if(S):
     print(res)
 else:
-    print(res - 1)
+    print(res - 1) #답이 없으면 -1 출력
+
+#출력 초과
