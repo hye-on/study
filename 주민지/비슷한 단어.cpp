50e00c79c06719482ba10d8f75f80e8274447ebb
minj-L <52521457+minj-L@users.noreply.github.com>
2021-07-11 20:53:35 +0900

    Update 비슷한 단어.cpp

--- 주민지/비슷한 단어.cpp
+++ 주민지/비슷한 단어.cpp
@@ -7,8 +7,10 @@ string str[101];
 
 int main() {
 	cin >> n;
-	for (i = 0; i < n; i++) cin >> str[i];
-	for (i = 0; i < n - 1; i++) for (j = i + 1; j < n; j++) {
+	for (i = 0; i < n; i++) 
+		cin >> str[i];
+	for (i = 0; i < n - 1; i++) 
+		for (j = i + 1; j < n; j++) {
 		int chk1[27] = { 0 }, chk2[27] = { 0 };
 		for (k = 0; k < str[i].length(); k++) {
 			int a = str[i][k] - 'a' + 1, b = str[j][k] - 'a' + 1;
