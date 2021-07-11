8197b17baac158172c930ad22126a30fbc334b97
minj-L <52521457+minj-L@users.noreply.github.com>
2021-07-11 20:38:39 +0900

    Update dfs,bfs.py

--- 주민지/dfs,bfs.py
+++ 주민지/dfs,bfs.py
@@ -1,28 +1,30 @@
-from sys import stdin
-n, m, v = map(int, stdin.readline().split())
-matrix = [[0] * (n + 1) for _ in range(n + 1)]
-for _ in range(m):
-    line = list(map(int, stdin.readline().split()))
-    matrix[line[0]][line[1]] = 1
-    matrix[line[1]][line[0]] = 1
+def dfs(v):
+    print(v, end=' ')
+    visit[v] = 1
+    for i in range(1, n + 1):
+        if visit[i] == 0 and s[v][i] == 1:
+            dfs(i)
 
-def bfs(start):
-    visited = [start]
-    queue = [start]
-    while queue:
-        n = queue.pop(0)
-        for c in range(len(matrix[start])):
-            if matrix[n][c] == 1 and (c not in visited):
-                visited.append(c)
-                queue.append(c)
-    return visited
+def bfs(v):
+    queue = [v]
+    visit[v] = 0
+    while(queue):
+        v = queue[0]
+        print(v, end=' ')
+        del queue[0]
+        for i in range(1, n + 1):
+            if visit[i] == 1 and s[v][i] == 1:
+                queue.append(i)
+                visit[i] = 0
 
-def dfs(start, visited):
-    visited += [start]
-    for c in range(len(matrix[start])):
-        if matrix[start][c] == 1 and (c not in visited):
-            dfs(c, visited)
-    return visited
-
-print(*dfs(v,[]))
-print(*bfs(v))
+n, m, v = map(int, input().split())
+s = [[0] * (n + 1) for i in range(n + 1)]
+visit = [0 for i in range(n + 1)]
+for i in range(m):
+    x, y = map(int, input().split())
+    s[x][y] = 1
+    s[y][x] = 1
+    
+dfs(v)
+print()
+bfs(v)
