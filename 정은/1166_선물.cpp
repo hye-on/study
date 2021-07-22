b67b512242636fcf980f1babc865c0266002e59d
jeong-1 <68230434+jeong-1@users.noreply.github.com>
2021-07-22 18:02:25 +0900

    Add files via upload

---
+++ 정은/1166_선물.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int n, l, w, h;
+	double low = 0, high = 1000000000, mid;
+	cin >> n >> l >> w >> h;
+	for (int i = 0; i < 10000; i++) {
+		mid = (low + high) / 2;
+		if ((long long)(l / mid)*(long long)(w / mid)*(long long)(h / mid) < n)
+			high = mid;
+		else
+			low = mid;
+	}
+	cout << fixed << setprecision(10) << low;
+}
