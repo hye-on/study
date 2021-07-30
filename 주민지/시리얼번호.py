b9026946640db5c52abfa0f17c252e6758663a49
minj-L <52521457+minj-L@users.noreply.github.com>
2021-07-30 23:00:02 +0900

    Create 시리얼번호.py

---
+++ 주민지/시리얼번호.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def sum_num(s):
+  res=0
+  for i in s:
+    if i.isdigit():
+      res += int(i)
+    retrun res
+    
+n = int(input())
+serial = [input().rstrip() for _ in range(n)]
+serial.sort(key=lambda x:(len(x), sum_num(x), x))
+for s in serial:
+  print(s)
