66d1e32a833d9c9bfe6b7ecb349ba9968504a85f
minj-L <52521457+minj-L@users.noreply.github.com>
2021-11-28 23:56:32 +0900

    Create 2790 F7.py

---
+++ 주민지/2790 F7.py
@@ -0,0 +1,21 @@
+import sys
+t =  int(sys.stdin.readline().rstrip())
+my_list = []
+for i in range(t) :
+  my_list.append(int(sys.stdin.readline().rstrip()))
+my_list = sorted(my_list)
+for i in range(len(my_list)) :
+  my_list[i] = my_list[i] + t
+  t -=1
+my_cnt = 0
+max_v = max(my_list)
+for i in range(len(my_list)) :
+  if my_list[i] >= max_v :
+    my_list += 1
+  t +=1
+  my_list[i+1] += t
+  
+if my_list[-1] > max_v :
+  print(my_cnt+1)
+else:
+  print(my_cnt)
