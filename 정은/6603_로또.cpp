104bac1b1ac95239fa1d8653303992107868a51b
jeong-1 <68230434+jeong-1@users.noreply.github.com>
2022-01-22 07:56:50 +0900

    Add files via upload

---
+++ 정은/6603_로또.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int s[13];
+int lotto[6];
+int k;
+
+void dfs(int start, int depth) {
+	if (depth == 6) {
+		for (int i = 0; i < 6; i++)
+			cout << lotto[i] << " ";
+		cout << "\n";
+		return;
+	}
+	
+	for (int i = start; i < k; i++) {
+		lotto[depth] = s[i];
+		dfs(i + 1, depth + 1);
+	}
+}
+
+int main() {
+	while (true) {
+		cin >> k;
+		if (k == 0) 
+			break;
+
+		for (int i = 0; i < k; i++)
+			cin >> s[i];
+
+		dfs(0, 0);
+		cout << "\n";
+	}
+}
